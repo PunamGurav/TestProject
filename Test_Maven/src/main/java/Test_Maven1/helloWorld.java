da1a280d65c4cfa2fa3b59953889fd956f71e7ae
vikash chandra thakur <45505515+VIKASHCHTH@users.noreply.github.com>
2022-01-04 15:22:36 +0530

    MY First Commit

---
+++ Test_Maven/src/main/java/Test_Maven1/helloWorld.java
@@ -0,0 +1,10 @@
+package Test_Maven1;
+
+public class helloWorld {
+
+
+	
+	public static void Test() {
+		System.out.println("Hello world");
+	}
+}
---
+++ Test_Maven/src/test/java/Test_2/Test_cmd.java
@@ -0,0 +1,10 @@
+package Test_2;
+
+import org.testng.annotations.Test;
+public class Test_cmd {
+
+	@Test
+	public static void Test() {
+		System.out.println("Hello world");
+	}
+}
